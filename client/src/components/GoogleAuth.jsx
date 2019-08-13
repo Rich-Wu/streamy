05dc9fb73b7ca1d3134fc3a75b29774da1ad78ac
Richard <richie1988@gmail.com>
2019-08-13 16:20:48 -0400

    moving to redux store

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -20,18 +20,30 @@ class GoogleAuth extends Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
   renderAuthButton() {
     if(this.state.isSignedIn === null) {
-      return (
-        <div>I don't know if we are signed in</div>
-      );
+      return null;
     } else if(this.state.isSignedIn) {
       return (
-        <div>I am signed in!</div>
+        <button className="ui red google button" onClick={this.onSignOutClick}>
+          <i className="google icon"></i>
+          Sign Out
+        </button>
       );
     } else {
       return (
-        <div>I am not signed in</div>
+        <button className="ui green google button" onClick={this.onSignInClick}>
+          <i className="google icon"></i>
+          Sign In with Google
+        </button>
       );
     }
   }
